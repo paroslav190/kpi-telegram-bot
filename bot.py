fdf115dc20d46c454ce45a25c325cfacc1ca9d0e
paroslav190 <paroslav190@gmail.com>
2026-09-02 23:37:33 +0300

    Update2 bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ import os
 # ================= НАЛАШТУВАННЯ =================
 EMAIL = "iasa-kn61@ukr.net"  # <--- ВПИШИ СВОЮ ПОШТУ
 PASSWORD = "avuGkSlgPL3LLovt"
-BOT_TOKEN = "8924622903:AAE9zOKnrKA4o2bVQfxciKgLrIY9xKL_1q8"
+BOT_TOKEN = "8924622903:AAFUUvctYBGm4w6d3DeSB3T6GwVY3GauT_c"
 CHAT_ID = "-1004391355458"
 MAIL_THREAD_ID = 7161
 # ================================================
